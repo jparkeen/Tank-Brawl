b026e5357dab2607651d49ba962662b71a585788
saengduean8 <happygoldfish8@gmail.com>
2017-07-18 22:34:42 -0700

    update TankWorld and TankObject

--- src/TankObject.java
+++ src/TankObject.java
@@ -1,5 +1,33 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 /**
  * Created by EmEm on 7/15/2017.
  */
-public class TankObject {
+public  class TankObject {
+
+    protected int x,y,health,speed,id;
+    String playerName;
+    protected BufferedImage image;
+    protected TankWorld tankWorld;
+
+
+    public TankObject(int x,int y,String imagePath,int id,String playerName,TankWorld tankWorld){
+        this.x = x;
+        this.y = y;
+        this.id = id;
+        this.playerName = playerName;
+        this.tankWorld = tankWorld;
+        this.image = loadImages.loadImages(imagePath);
+
+    }
+
+    public void update(){
+
+    }
+
+    public void drawImage(Graphics g){
+
+    }
+
 }
--- src/TankWorld.java
+++ src/TankWorld.java
@@ -10,18 +10,22 @@ import java.awt.Graphics;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Dimension;
+import javax.imageio.ImageIO;
 
 
 
 // Task: This TankWorld class will hold all the main code of the tank game
 public class TankWorld extends JPanel implements Runnable {
     private Thread thread;
-    int x = 0, y = 0;
+    int x = 0, y = 0,add = 0;
     public String title;
     public int width, height;
     private Graphics g;
-    private BufferedImage Image;
-    private KeysControl keysControl;
+    private BufferedImage Image,background;
+    static TankObject tankOne,tankTwo;
+   // static GameEvent gameEvent;
+
+    //private KeysControl keysControl;
 
 
     /*public TankWorld(String title, int width, int height){
@@ -33,17 +37,21 @@ public class TankWorld extends JPanel implements Runnable {
     }*/
 
     public void init() {
+        setFocusable(true);
+        background = loadImages.loadImages("/resources/Background.png");
 
-        Image = loadImages.loadImages("/resources/Tank_blue_basic_strip60-0-9.png");
+        this.tankOne = new TankObject(100, 50, "/resources/Tank_blue_basic_strip60-0-30.png", 1, "Tank 1",this);
+        this.tankTwo = new TankObject(150, 150, "/resources/Tank_blue_basic_strip60-0-9.png", 2, "Tank 2",this);
+
+       // gameEvent = new GameEvent();
+        //keysControl = new KeysControl();
 
 
 
 
     }
 
-    public KeysControl getKeysControl() {
-        return keysControl;
-    }
+
 
 
     public void start() {
@@ -52,18 +60,23 @@ public class TankWorld extends JPanel implements Runnable {
     }
 
     public void paint(Graphics g) {
-        x++;
-        y++;
-
-        g.setColor(Color.black);
-        g.fillRect(x, y, 32, 32);
-        g.drawImage(Image,32,32,null);
+        //this is for testing
+        if(this.tankOne.x == 100)
+            this.add = -1;
+        if(this.tankOne.x ==0)
+            this.add = 1;
+        this.tankOne.x += add;
+        this.tankTwo.y += add;
+        g.clearRect(0, 0, 600, 600);
+        g.drawImage(background, 0, 0, this);
+        g.drawImage(this.tankOne.image, this.tankOne.x, this.tankOne.y, this);
+        g.drawImage(this.tankTwo.image, this.tankTwo.x, this.tankTwo.y, this);
 
 
     }
 
     private void update() {
-        keysControl.update();
+        //keysControl.update();
     }
 
     public void run() { // took from air stirke project. Temporary code
@@ -91,9 +104,9 @@ public class TankWorld extends JPanel implements Runnable {
         f.pack();
         f.setSize(600, 600);
         f.setResizable(false);
-       // f.setLocationByPlatform(true);
+        f.setLocationByPlatform(true);
         f.setVisible(true);
-        //f.setLocationRelativeTo(null);
+        f.setLocationRelativeTo(null);
         mainPanel.start();
 
     }
