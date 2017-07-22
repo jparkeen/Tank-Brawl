00bcfa63232b177618b5433e6a1f9acd410d58d0
Richa Mirashi <richamirashi24@gmail.com>
2017-07-21 21:30:30 -0700

    Updated KeysControl.java class and changed keys control for both the tanks

--- src/components/KeysControl.java
+++ src/components/KeysControl.java
@@ -20,43 +20,43 @@ public class KeysControl extends KeyAdapter {
         int keysCode = e.getKeyCode();
         if (keysCode == KeyEvent.VK_UP) {
 
-            TankWorld.tank1.y -= Globals.BLOCK_SIZE;
-            TankWorld.tank1.orientation = TankOrientation.TOP;
-
-        } else if (keysCode == KeyEvent.VK_DOWN) {
-
-            TankWorld.tank1.y += Globals.BLOCK_SIZE;
-            TankWorld.tank1.orientation = TankOrientation.DOWN;
-
-        } else if (keysCode == KeyEvent.VK_LEFT) {
-
-            TankWorld.tank1.x -= Globals.BLOCK_SIZE;
-            TankWorld.tank1.orientation = TankOrientation.LEFT;
-
-        } else if (keysCode == KeyEvent.VK_RIGHT) {
-
-            TankWorld.tank1.x += Globals.BLOCK_SIZE;
-            TankWorld.tank1.orientation = TankOrientation.RIGHT;
-
-        } else if (keysCode == KeyEvent.VK_W) {
-
             TankWorld.tank2.y -= Globals.BLOCK_SIZE;
             TankWorld.tank2.orientation = TankOrientation.TOP;
 
-        } else if (keysCode == KeyEvent.VK_S) {
+        } else if (keysCode == KeyEvent.VK_DOWN) {
 
             TankWorld.tank2.y += Globals.BLOCK_SIZE;
             TankWorld.tank2.orientation = TankOrientation.DOWN;
 
-        } else if (keysCode == KeyEvent.VK_A) {
+        } else if (keysCode == KeyEvent.VK_LEFT) {
 
             TankWorld.tank2.x -= Globals.BLOCK_SIZE;
             TankWorld.tank2.orientation = TankOrientation.LEFT;
 
-        } else if (keysCode == KeyEvent.VK_D) {
+        } else if (keysCode == KeyEvent.VK_RIGHT) {
 
             TankWorld.tank2.x += Globals.BLOCK_SIZE;
             TankWorld.tank2.orientation = TankOrientation.RIGHT;
+
+        } else if (keysCode == KeyEvent.VK_W) {
+
+            TankWorld.tank1.y -= Globals.BLOCK_SIZE;
+            TankWorld.tank1.orientation = TankOrientation.TOP;
+
+        } else if (keysCode == KeyEvent.VK_S) {
+
+            TankWorld.tank1.y += Globals.BLOCK_SIZE;
+            TankWorld.tank1.orientation = TankOrientation.DOWN;
+
+        } else if (keysCode == KeyEvent.VK_A) {
+
+            TankWorld.tank1.x -= Globals.BLOCK_SIZE;
+            TankWorld.tank1.orientation = TankOrientation.LEFT;
+
+        } else if (keysCode == KeyEvent.VK_D) {
+
+            TankWorld.tank1.x += Globals.BLOCK_SIZE;
+            TankWorld.tank1.orientation = TankOrientation.RIGHT;
 
         }
     }
